505688b0bbc5975111515117aecf0321ab031bab
nerayan <r.golovchuk@gmail.com>
2021-06-14 11:48:56 +0300

    np shipping update

---
+++ wp-content/plugins/wc-ukr-shipping/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '507a711c82d989bb675a5d67be273a06dcfb9dbc',
+    'name' => 'kirillbdev/wc-ukr-shipping',
+  ),
+  'versions' => 
+  array (
+    'kirillbdev/wc-ukr-shipping' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '507a711c82d989bb675a5d67be273a06dcfb9dbc',
+    ),
+    'kirillbdev/wcus-core' => 
+    array (
+      'pretty_version' => 'v1.0.4',
+      'version' => '1.0.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '83ee876fdaa82deb8e26d7cc23e3aa0e78f32366',
+    ),
+  ),
+);
